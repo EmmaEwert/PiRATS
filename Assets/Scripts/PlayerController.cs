5e5187b10f89caea4aeaec053a2dc9b27f47fd9d
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-17 16:58:53 +0200

    Add mouse aim, indicator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@
 public class PlayerController : MonoBehaviour {
 	const float Speed = 5f;
 
+	public Transform aimIndicator;
+
 #pragma warning disable 0108
 	Rigidbody2D	rigidbody => GetComponent<Rigidbody2D>();
 #pragma warning restore 0108
 
     void Update() {
 		var Δt = Time.deltaTime;
+
         var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 		movement = Vector3.ClampMagnitude(movement, 1f);
 		movement = Quaternion.Euler(30f, 0f, 0f) * movement;
 		rigidbody.MovePosition(rigidbody.position + new Vector2(movement.x, movement.y) * Δt * Speed);
+
+		var mouse = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		var aim = Vector2.SignedAngle(Vector2.up, mouse - Vector3.one * 0.5f);
+		aimIndicator.localRotation = Quaternion.Euler(60f, 0f, aim);
     }
 }
