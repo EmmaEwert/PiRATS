0a0dfe7ba28655bc6894c53c2b14b647e134164d
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-18 04:12:51 +0200

    Make Rat Sparrow's tail movements smoother

--- Assets/Scripts/RatSparrow.cs
+++ Assets/Scripts/RatSparrow.cs
@@ -26,7 +26,7 @@ public class RatSparrow : MonoBehaviour {
 
 		for (var i = 0; i < tail.childCount; ++i) {
 			var child = tail.GetChild(i);
-			var tailPosition = new Vector3(Mathf.Cos(t + i / 2f) * i / (tail.childCount - 1f), (Mathf.Sin(t + i / 3f) * 0.5f + 0.5f) * i / (tail.childCount - 7f), -0.25f * i);
+			var tailPosition = new Vector3(Mathf.Cos(t + i / 2f) * Mathf.Pow(i, 1f / 1.5f) / (tail.childCount - 5f), (Mathf.Sin(t + i / 3f) * 0.5f + 0.5f) * Mathf.Pow(i, 1f / 1.5f) / (tail.childCount - 11f), -0.25f * Mathf.Pow(i, 1f / 1.5f));
 			var attackPosition = Vector3.ClampMagnitude(new Vector3(attackX * 1f, -0.5f * i / 2f, -0.25f * i), i / 1.5f);
 			child.localPosition = Vector3.Lerp(tailPosition, attackPosition, Mathf.Pow(attack * Mathf.Pow(((tail.childCount - i) / (tail.childCount - 1f)), 1f / 32f), 16f));
 			var shadow = child.Find("Shadow");
