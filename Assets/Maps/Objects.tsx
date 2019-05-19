6d757b80fda8afd78e2b6a159a32cd467fa56885
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-19 03:26:23 +0200

    Add too much stuff without committing

---
+++ Assets/Maps/Objects.tsx
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Objects" tilewidth="160" tileheight="96" tilecount="31" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Big Rock.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Small Rock.png"/>
+ </tile>
+ <tile id="2">
+  <image width="96" height="96" source="Lab House.png"/>
+ </tile>
+ <tile id="3">
+  <image width="160" height="96" source="Red House.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="64" source="Lamp Post.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="64" source="Big House.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="48" source="Shop Sign.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="48" source="Potions Sign.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Anchor.png"/>
+ </tile>
+ <tile id="9">
+  <image width="96" height="48" source="Small House.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="64" source="Green Tree.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="64" source="Yellow Tree.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="Small Green Bush.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="Big Green Bush.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="Big Yellow Bush.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="Small Yellow Bush.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="Big Sword.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="Small Barrel.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="Fish Barrel.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="Double Barrel Flask.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="Table Sword.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="Fence Bottom.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="Fence Middle.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="Fence Top.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="Fence Left.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="Fence Down Left.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="Fence Right.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="Fence Center.png"/>
+ </tile>
+ <tile id="28">
+  <image width="96" height="64" source="Small Red House.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="Sign.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="Skull.png"/>
+ </tile>
+</tileset>
