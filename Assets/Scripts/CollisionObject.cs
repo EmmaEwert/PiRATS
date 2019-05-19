f010089eee16f3bfbb6ba97ba8d8bd0dfcee90e9
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-19 22:59:08 +0200

    Add collision objects

---
+++ Assets/Scripts/CollisionObject.cs
@@ -0,0 +1,11 @@
+﻿using KITTY;
+using UnityEngine;
+
+public class CollisionObject : MonoBehaviour {
+	[TiledProperty] public float width = 2;
+	[TiledProperty] public float height = 1;
+
+    void Start() {
+        transform.localScale = new Vector3(width, height, 1);
+    }
+}
